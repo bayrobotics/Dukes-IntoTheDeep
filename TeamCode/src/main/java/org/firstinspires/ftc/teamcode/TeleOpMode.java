506949cd9c7d08c9d6110c46294b8739f441275b
Henry O'Connor <henryuoconnor@gmail.com>
2024-09-14 13:40:15 -0500

    Added RobotComponents class for real, added TeleOp for chassis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 
 @TeleOp()
@@ -10,6 +12,17 @@ public class TeleOpMode extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        
+
+        Chassis chassis = new Chassis(hardwareMap.get(DcMotor.class, "front_left"), DcMotor.Direction.FORWARD,
+                hardwareMap.get(DcMotor.class, "front_right"), DcMotor.Direction.FORWARD,
+                hardwareMap.get(DcMotor.class, "back_right"),DcMotor.Direction.FORWARD,
+                hardwareMap.get(DcMotor.class, "back_left"), DcMotor.Direction.REVERSE);
+
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            chassis.updateState(gamepad1, gamepad2);
+        }
     }
 }
