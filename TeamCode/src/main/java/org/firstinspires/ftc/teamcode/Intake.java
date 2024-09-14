a04212dbef3cd09c88c5dc2770a8f11adf1257c0
Henry O'Connor <henryuoconnor@gmail.com>
2024-09-14 15:02:39 -0500

    Created classes for the future intake and lift mechanisms

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class Intake extends RobotComponent{
+
+    public Intake() {
+
+    }
+
+    public void updateState(Gamepad gamepad, Gamepad gamepad2) {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class Lift extends RobotComponent{
+
+    public Lift() {
+
+    }
+
+    public void updateState(Gamepad gamepad, Gamepad gamepad2) {
+
+    }
+}
